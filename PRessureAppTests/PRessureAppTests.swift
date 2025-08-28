159e3266444defe07cbbcdc42bfa5df701136a1a
dillonsherling <dillon.sherling@wsu.edu>
2025-08-28 14:20:29 -0700

    Initial Commit

---
+++ PRessureAppTests/PRessureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PRessureAppTests.swift
+//  PRessureAppTests
+//
+//  Created by dillon sherling on 8/28/25.
+//
+
+import Testing
+@testable import PRessureApp
+
+struct PRessureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
